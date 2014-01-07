842a54dd38b5de13922c9b982dc875df38235cde
Luis Alves <ljalvs@gmail.com>
2014-01-07 00:09:14 +0000

    saa716x: Fix to build FF cards in newer kernels

--- drivers/media/pci/saa716x/saa716x_ff_cmd.h
+++ drivers/media/pci/saa716x/saa716x_ff_cmd.h
@@ -1,6 +1,24 @@
 #ifndef __SAA716x_FF_CMD_H
 #define __SAA716x_FF_CMD_H
 
+#if !defined OSD_RAW_CMD
+typedef struct osd_raw_cmd_s {
+	const void *cmd_data;
+	int cmd_len;
+	void *result_data;
+	int result_len;
+} osd_raw_cmd_t;
+
+typedef struct osd_raw_data_s {
+	const void *data_buffer;
+	int data_length;
+	int data_handle;
+} osd_raw_data_t;
+
+#define OSD_RAW_CMD            _IOWR('o', 162, osd_raw_cmd_t)
+#define OSD_RAW_DATA           _IOWR('o', 163, osd_raw_data_t)
+#endif
+
 extern int sti7109_cmd_init(struct sti7109_dev *sti7109);
 extern int sti7109_raw_cmd(struct sti7109_dev * sti7109,
 			   osd_raw_cmd_t * cmd);
