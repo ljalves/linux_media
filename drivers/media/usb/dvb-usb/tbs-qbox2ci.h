899205de100e95c90a4d3ec7956f637e45232f32
CrazyCat <crazycat69@narod.ru>
2014-02-22 22:12:30 +0200

    Support for TBS QBox(5910), QBox2(5920), QBoxS2(5928), QBox2CI(5980), 5925, 5880.

---
+++ drivers/media/usb/dvb-usb/tbs-qbox2ci.h
@@ -0,0 +1,8 @@
+#ifndef _TBSQBOX2CI_H_
+#define _TBSQBOX2CI_H_
+
+#define DVB_USB_LOG_PREFIX "tbsqbox2ci"
+#include "dvb-usb.h"
+
+#define deb_xfer(args...) dprintk(dvb_usb_tbsqbox2ci_debug, 0x02, args)
+#endif
