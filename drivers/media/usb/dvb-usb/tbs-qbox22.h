3414a64eb6ad9efecd76a9f695ad61cbb2646017
Luis Alves <ljalvs@gmail.com>
2014-07-20 00:37:39 +0100

    TBS5922: Added card. Testing needed.

---
+++ drivers/media/usb/dvb-usb/tbs-qbox22.h
@@ -0,0 +1,8 @@
+#ifndef _TBSQBOX22_H_
+#define _TBSQBOX22_H_
+
+#define DVB_USB_LOG_PREFIX "tbsqbox22"
+#include "dvb-usb.h"
+
+#define deb_xfer(args...) dprintk(dvb_usb_tbsqbox22_debug, 0x02, args)
+#endif
