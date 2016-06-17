958e05a260e7f80e5cc25795fd7d9cf93c56b4a9
Luis Alves <ljalvs@gmail.com>
2016-06-17 19:17:29 +0100

    si2157: Wake tuner on probe

--- drivers/media/tuners/si2157.c
+++ drivers/media/tuners/si2157.c
@@ -436,8 +436,15 @@ static int si2157_probe(struct i2c_client *client,
 	INIT_DELAYED_WORK(&dev->stat_work, si2157_stat_work);
 
 	/* check if the tuner is there */
-	cmd.wlen = 0;
-	cmd.rlen = 1;
+        /* wake tuner */
+	if (dev->chiptype == SI2157_CHIPTYPE_SI2146) {
+		memcpy(cmd.args, "\xc0\x05\x01\x00\x00\x0b\x00\x00\x01", 9);
+		cmd.wlen = 9;
+	} else {
+		memcpy(cmd.args, "\xc0\x00\x0c\x00\x00\x01\x01\x01\x01\x01\x01\x02\x00\x00\x01", 15);
+		cmd.wlen = 15;
+	}
+        cmd.rlen = 1;
 	ret = si2157_cmd_execute(client, &cmd);
 	if (ret)
 		goto err_kfree;
